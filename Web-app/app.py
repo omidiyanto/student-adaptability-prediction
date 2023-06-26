a5788506c8df0b23ae56a86b8a6d46f3105dc73b
omidiyanto <2001010021@student.umrah.ac.id>
2023-06-26 15:00:15 +0700

    Update app.py

--- Web-app/app.py
+++ Web-app/app.py
@@ -63,7 +63,7 @@ def get_access_token():
         return None
 
 def get_prediction(access_token, *input_values):
-    url = "https://eu-gb.ml.cloud.ibm.com/ml/v4/deployments/7e020044-32d8-44ec-aa80-c50fd87be553/predictions?version=2021-05-01"
+    url = "https://eu-gb.ml.cloud.ibm.com/ml/v4/deployments/afd77ce5-67f9-4cef-b3ba-85ff96a34fd4/predictions?version=2021-05-01"
     headers = CaseInsensitiveDict()
     headers["Content-Type"] = "application/json"
     headers["Accept"] = "application/json"
