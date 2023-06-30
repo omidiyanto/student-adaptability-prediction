1af707551189a7dde79c9892d79f1b1e4576ce07
omidiyanto <2001010021@student.umrah.ac.id>
2023-06-30 21:43:08 +0700

    Update app.py

--- Web-app/app.py
+++ Web-app/app.py
@@ -93,7 +93,7 @@ def get_prediction(access_token, *input_values):
     else:
         return None
     
-#Run code below if you want to host it locally
+#Uncomment code below if you want to host it locally
 #if __name__ == '__main__':
 #    app.run(debug=False,host='0.0.0.0')
 
